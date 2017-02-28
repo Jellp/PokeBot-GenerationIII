e15886993b95d8f3dddf2bc2d525ae932f8e1791
Jellp <officialjellp@gmail.com>
2017-02-28 19:39:10 +0100

    My head hurts

--- main.lua
+++ main.lua
@@ -205,13 +205,23 @@ while true do
 				Bridge.liveSplit()
 				running = true
 			end
+			--This shit crashes or gives wrong values, figure it out later?
+			local Cantmove = Memory.value("player", "cantmove")
+			print(Cantmove)
+			print("CAN I BATTLE?")
+			local battlemenu = Memory.value("battle", "menu")
+			print(battlemenu)
+			print("AM I REALLY IN A BATTLE?")
 			local battleState = Memory.value("game", "battle") --3/4 variabelen om te zien of we een battle hebben, CanMove (0F2C), Battle - menu (>5) (5D60), CanFlee (2) (5DF0), Battling (090E).
-			Control.encounter(battleState)
-			--local curr_hp = Pokemon.index(0, "hp")
-			--if curr_hp == 0 and not Control.canDie() and Pokemon.index(0) > 0 then
-			--	Strategies.death(currentMap)
-			--elseif Walk.strategy then
-			if Walk.strategy then
+			print(battleState)
+			if battleState == 1 or battlemenu > 5 then --We b battlin, battlemenu can go up to >=45
+				print("TAKING OVER A BATTLE")
+				Control.encounter(battleState)
+				--local curr_hp = Pokemon.index(0, "hp")
+				--if curr_hp == 0 and not Control.canDie() and Pokemon.index(0) > 0 then
+				--	Strategies.death(currentMap)
+			elseif Walk.strategy then
+			--if Walk.strategy then
 				if Strategies.execute(Walk.strategy) then
 					Walk.traverse(currentMap)
 				end
